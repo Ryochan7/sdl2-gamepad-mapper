fc28d65fea5b299800881747139f1ce80457e96e
Travis Nickles <ryochan7@protonmail.com>
2024-05-10 14:42:41 -0500

    Removed no longer needed code from main

--- main.cpp
+++ main.cpp
@@ -12,15 +12,6 @@
 #include <QStandardPaths>
 #include <QSettings>
 
-//#include "registerqmltypes.h"
-
-
-/*void RegisterQMLTypes()
-{
-    RegisterQMLTypes::RegisterTypesForQML();
-}
-*/
-
 int main(int argc, char *argv[])
 {
     //QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
@@ -44,8 +35,6 @@ int main(int argc, char *argv[])
     {
         QQuickStyle::setStyle("Fusion");
     }
-
-    //RegisterQMLTypes();
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
