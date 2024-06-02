957a15f04ce12269e4570789a1195a9fcd1ed7da
Travis Nickles <ryochan7@protonmail.com>
2024-06-01 21:39:55 -0500

    Figured out how to build translations from cmake

--- sdl2-gamepad-mapper_en_US.ts
+++ sdl2-gamepad-mapper_en_US.ts
@@ -1,3 +1,166 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="SDL2Delv_en_US"></TS>
+<TS version="2.1">
+<context>
+    <name>MainViewBackend</name>
+    <message>
+        <location filename="sdl2mappercomponents/mainviewbackend.cpp" line="160"/>
+        <source>The GUID for &quot;%1&quot; is &quot;%2&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sdl2mappercomponents/mainviewbackend.cpp" line="172"/>
+        <source>Compiled with SDL %1, DLL version %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sdl2mappercomponents/mainviewbackend.cpp" line="276"/>
+        <source>JSON Parsing Failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="23"/>
+        <source>SDL2 Gamepad Mapper</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="29"/>
+        <source>Copying GUID of &quot;%1&quot; to clipboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="30"/>
+        <source>Copying GUID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="31"/>
+        <source>Copying SDL Mapping string of &quot;%1&quot; to clipboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="32"/>
+        <source>Copying SDL Mapping string</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="33"/>
+        <source>Saved mapping to file: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="72"/>
+        <source>Save Mapping?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="91"/>
+        <source>Save Mapping with provided name?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="100"/>
+        <source>Mapping Name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="140"/>
+        <source>Mapping string: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="336"/>
+        <source>Copy Gamepad GUID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="349"/>
+        <source>The GUID for &quot;%1&quot; is &quot;%2&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="359"/>
+        <source>Create A New Mapping</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="373"/>
+        <source>Copy Mapping String</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="386"/>
+        <source>Mapping string for &quot;%1&quot; is &quot;%2&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="403"/>
+        <source>Skip</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="413"/>
+        <source>Previous</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="423"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="447"/>
+        <source>Set Mapping As Environmental Variable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="454"/>
+        <source>Setting environmental variable &quot;SDL_GAMECONTROLLERCONFIG&quot; to &quot;%1&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="464"/>
+        <source>Reset Environmental Variable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="470"/>
+        <source>Reseting environmental variable &quot;SDL_GAMECONTROLLERCONFIG&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="497"/>
+        <location filename="main.qml" line="511"/>
+        <source>Found %0 gamepad(s)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="503"/>
+        <source>Loading any existing mappings into SDL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="506"/>
+        <source>Scan for controllers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="531"/>
+        <source>(mapping available)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="544"/>
+        <source>Environmental variable &quot;SDL_GAMECONTROLLERCONFIG&quot; was found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="548"/>
+        <source>Environmental variable &quot;SDL_GAMECONTROLLERCONFIG&quot; was not found</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
